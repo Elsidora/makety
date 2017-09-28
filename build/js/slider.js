c89bc81ebfaf3dcd563485963e0288317da08eb5
Ella Sidorova <ella-sidorova@mail.ru>
2017-09-28 16:40:35 +0300

    start

---
+++ build/js/slider.js
@@ -0,0 +1,52 @@
+'use strict';
+
+let slides = document.querySelectorAll('.slides__slide'),
+	currentSlide = 0,
+	slideInterval = setInterval(nextSlide,4000),
+	playing = true,
+	pauseButton = document.getElementById('pause'),
+	next = document.getElementById('next'),
+	previous = document.getElementById('previous');
+
+function nextSlide() {
+ goToSlide(currentSlide+1);
+}
+
+function previousSlide() {
+ goToSlide(currentSlide-1);
+}
+
+function goToSlide(n) {
+ slides[currentSlide].className = 'slides__slide';
+ currentSlide = (n+slides.length)%slides.length;
+ slides[currentSlide].className = 'slides__slide showing';
+}
+
+function pauseSlideshow() {
+ pauseButton.innerHTML = 'Play';
+ playing = false;
+ clearInterval(slideInterval);
+}
+
+function playSlideshow() {
+ pauseButton.innerHTML = 'Pause';
+ playing = true;
+ slideInterval = setInterval(nextSlide,4000);
+}
+
+pauseButton.onclick = ()=> {
+ if(playing) {
+ pauseSlideshow();
+  } else {
+ playSlideshow();
+  }
+};
+
+next.onclick = () => {
+ pauseSlideshow();
+ nextSlide();
+};
+previous.onclick = () => {
+ pauseSlideshow();
+ previousSlide();
+};
